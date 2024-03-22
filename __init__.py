ddebb8c29494215a13e6e74975b45ffd00c6752e
Marc Dirven <marcdirven1996@outlook.com>
2024-03-22 18:32:56 +0100

    add necesairy functions only

--- __init__.py
+++ __init__.py
@@ -1,6 +1,1 @@
-from dynamic_prices import *
-from dynamic_pricing_parser import *
-from parse_utils import *
-from vrijopnaam import *
-from vrijopnaam_prices import *
-from vrijopnaam_session import *
+from vrijopnaam_prices import get_prices
