44558355d923c0e475ce6bca51d57207561bbf55
Marc Dirven <github@mdy.mozmail.com>
2024-03-27 21:41:59 +0100

    place prefixes back

--- vrijopnaam_prices/__init__.py
+++ vrijopnaam_prices/__init__.py
@@ -1,8 +1,8 @@
 from typing import Tuple
 
-from prices import get_prices
-from time_bounded_price import TimeBoundedPrice
-from dynamic_prices import DynamicGasPrices, DynamicElectricityPrices, DynamicPrices
+from vrijopnaam_prices.prices import get_prices
+from vrijopnaam_prices.time_bounded_price import TimeBoundedPrice
+from vrijopnaam_prices.dynamic_prices import DynamicGasPrices, DynamicElectricityPrices, DynamicPrices
 
 __all__: Tuple[str, ...] = (
     "get_prices", "TimeBoundedPrice", "DynamicGasPrices", "DynamicElectricityPrices", "DynamicPrices"
